034abfc38123fa080c0ba9d3c289fe0e5b4d68dc
ncs14 <nicole.solomon@dm.duke.edu>
2024-07-31 20:16:54 -0400

    Functions added

---
+++ R/iccmulti.R
@@ -0,0 +1,296 @@
+# ICC estimation function that runs on (any) number of categories (2+)
+iccmulti = function(cid, y, data, alpha=0.05, method=c("rm","mom"),
+                    binmethod = c("aov", "aovs", "keq", "kpr", "keqs",
+                                  "kprs", "stab", "ub", "fc", "mak", "peq",
+                                  "pgp", "ppr", "rm", "lin", "sim"),
+                    ci.type = c("aov", "wal", "fc", "peq", "rm"),
+                    kappa = 0.45, nAGQ = 1, M = 1000, nowarnings=FALSE){
+  # cid       = Column name indicating cluster id in dataframe data
+  # y         = Column name indicating categorical response in data frame `data'
+  # data      = Dataframe containing `cid' and `y'
+  # alpha     = Significance level for confidence interval computation. Default
+  #             value is 0.05.
+  # method    = Method used to compute categorical ICC. A single method or
+  #             multiple methods can be specified. Default is both resampling
+  #             & moments methods. See Details of iccmult::iccmulti() for more.
+  # binmethod = Method used to compute binary ICC. A single or multiple methods
+  #             can be specified. By default, all 16 methods will be used. See
+  #             details of iccBIN::iccbin() for more.
+  # ci.type   = Type of confidence interval to be computed for binary ICC. By
+  #             default, all 5 types will be reported. See details of
+  #             iccBIN::iccbin() for more.
+  # kappa     = Value of Kappa to be used in computing Stabilized ICC when the
+  #             binary response method stab is chosen. Default value is 0.45.
+  # nAGQ      = An integer scaler, as in glmer function of package lme4,
+  #             denoting the number of points per axis for evaluating the
+  #             adaptive Gauss-Hermite approximation to the log-likelihood.
+  #             Used when the binary response method lin is chosen. Default
+  #             value is 1.
+  # M         = Number of Monte Carlo replicates used in ICC computation method
+  #             sim. Default is 1000.
+  # nowarnings = flag to turn off warnings. Default is False.
+
+  ##~~~ NOTE: ICCbin::iccbin may return an error between lmer and Matrix package
+  ##~~~ when method=lin or sim. Run the following install call to avoid this
+  ##~~~ error:
+  ##~~~ > install.packages("lme4", type = "source")
+
+  require("ICCbin")
+  require("lme4")
+  require("dirmult")
+  if(nowarnings==TRUE) options(warn=-1)
+
+  CALL <- match.call()
+  ic <- list(cid = substitute(cid), y = substitute(y))
+  if (is.character(ic$y)) {
+    if (missing(data))
+      stop("Supply either the unqouted name of an object containing 'y' or supply both 'data' and then 'y' as an unquoted column name to 'data'")
+    ic$y <- eval(as.name(y), data, parent.frame())
+  }
+  if (is.name(ic$y))
+    ic$y <- eval(ic$y, data, parent.frame())
+  if (is.call(ic$y))
+    ic$y <- eval(ic$y, data, parent.frame())
+  if (is.character(ic$y))
+    ic$y <- eval(as.name(ic$y), data, parent.frame())
+  if (is.character(ic$cid)) {
+    if (missing(data))
+      stop("Supply either the unquoted name of an object containing 'cid' or supply both 'data' and then 'cid' as an unquoted column name to 'data'")
+    ic$cid <- eval(as.name(cid), data, parent.frame())
+  }
+  if (is.name(ic$cid))
+    ic$cid <- eval(ic$cid, data, parent.frame())
+  if (is.call(ic$cid))
+    ic$cid <- eval(ic$cid, data, parent.frame())
+  if (is.character(ic$cid) && length(ic$cid) == 1)
+    ic$cid <- eval(as.name(ic$cid), data, parent.frame())
+  dt <- data.frame(ic)
+  dt <- na.omit(dt)
+  k <- length(unique(dt$cid))
+  if (!is.null(attributes(dt)$na.action)) {
+    warning(cat("NAs removed from data rows:\n", unclass(attributes(dt)$na.action),
+                "\n"))
+  }
+  if (!is.factor(dt$cid)) {
+    warning("'cid' has been coerced to a factor")
+    dt$cid <- as.factor(dt$cid)
+  }
+  else {
+    if (length(levels(dt$cid)) > k) {
+      dt$x <- factor(as.character(dt$cid), levels = unique(dt$cid))
+      warning("Missing levels of 'cid' have been removed")
+    }
+  }
+
+  ### Function to capture unique warnings
+  saveWarnings <- function(expr) {
+    warnings <- NULL
+    wHandler <- function(w) {
+      warnings <<- c(warnings, conditionMessage(w))
+      invokeRestart("muffleWarning")
+    }
+    result <- withCallingHandlers(expr, warning = wHandler)
+    list(result = result, warnings = warnings)
+  }
+
+  # Order records by cluster
+  cordata = dt[order(dt$cid),]
+  # convert factor outcome
+  if(is.factor(cordata$y)) cordata$y = as.numeric(cordata$y)
+  # Pull unique variable values
+  yvals = sort(unique(cordata$y))
+
+  ncat = length(yvals)
+
+  rho = se = NULL
+
+  if(ncat>=3){
+    if("rm" %in% method){
+      warns=NULL
+
+      for(i in 1:ncat){
+        # ICC for component i vs others
+        cordatap = cordata;
+        cordatap$ysub = ifelse(cordatap$y==yvals[i],1,0)
+
+        # Save iccbin results and warnings
+        iball <- saveWarnings({
+          ICCbin::iccbin(cid, ysub, data=cordatap, method="rm", ci.type="rm", alpha=alpha)
+        })
+        # iccbin warnings
+        warns=c(warns,iball$warnings)
+        # iccbin estimates
+        ib = iball$result
+
+        # Check if ICC estimable:
+        if(ib$estimates[,"ICC"]=="-"){ rho = c(rho,NA) }else{ rho = c(rho,ib$estimates[,"ICC"])}
+        # rho = ifelse(ib$estimates[,"ICC"]!="-",c(rho,ib$estimates[,"ICC"]),c(rho,NA))
+
+        # Check if CI estimable:
+        sumci = sum(ib$ci[,c("LowerCI","UpperCI")]==rep("-",2))
+        if(sumci>0){
+          ci=c(0,0);
+          if(ib$ci[,"LowerCI"]=="-"){ ci[1] = NA }else{ ci[1] = ib$ci[,"LowerCI"] }
+          if(ib$ci[,"UpperCI"]=="-"){ ci[2] = NA }else{ ci[2] = ib$ci[,"UpperCI"] }
+          se = NA
+        }
+        else{
+          ci = as.numeric(ib$ci[,c("LowerCI","UpperCI")])
+          se = c(se,(ci[2]-ci[1])/(2*qnorm(1-alpha/2)))
+        }
+      }
+
+      if(ncat>3){
+        for(j in 2:floor(ncat/2)){
+          combos = gtools::permutations(2,ncat,c(0,1),rep=T)
+          combos = combos[which(rowSums(combos)==j),]
+          if(j==floor(ncat/2) & (ncat%%2)==0) combos = combos[(nrow(combos)/2+1):nrow(combos),]
+
+          for(k in 1:nrow(combos)){
+            pick = c(1:ncat)[which(combos[k,]>0)]
+            # ICC for (2+)-categories vs others
+            cordatap = cordata;
+            cordatap$ysub = ifelse(cordatap$y%in%yvals[pick],1,0)
+
+            # Save iccbin results and warnings
+            iball <- saveWarnings({
+              ICCbin::iccbin(cid, ysub, data=cordatap, method="rm", ci.type="rm", alpha=alpha)
+            })
+            # iccbin warnings
+            warns=c(warns,iball$warnings)
+            # iccbin estimates
+            ib = iball$result
+
+            # Check if ICC estimable:
+            rho = ifelse(ib$estimates[,"ICC"]!="-",c(rho,ib$estimates[,"ICC"]),c(rho,NA))
+
+            # Check if CI estimable:
+            sumci = sum(ib$ci[,c("LowerCI","UpperCI")]==rep("-",2))
+
+            if(sumci>0){
+              ci=c(0,0);
+              ci[which(ib$ci[,c("LowerCI","UpperCI")]=="-")]=NA;
+              ci[which(ib$ci[,c("LowerCI","UpperCI")]!="-")] = ib$ci[,c("LowerCI","UpperCI")][which(ib$ci[,c("LowerCI","UpperCI")]!="-")]
+              se = NA
+            }
+            else{
+              ci = as.numeric(ib$ci[,c("LowerCI","UpperCI")])
+              se = c(se,(ci[2]-ci[1])/(2*qnorm(1-alpha/2)))
+            }
+          }
+        }
+      }
+
+      # # # # Change capitalization of warnings # # # #
+      make_lower <- function(text, exception) {
+        words <- unlist(strsplit(text, " "))
+        words <- ifelse(words %in% exception | words == words[1],
+                        words, tolower(words))
+        paste(words, collapse = " ")
+      }
+      multiwarns = unname( sapply(unique(warns),function(x) make_lower(x,
+                                                                       c("ICC","'Resampling'","'Resampling"))) )
+      multiwarns = paste0(multiwarns," for at least one categorical calculation")
+      if(nowarnings==FALSE & is.null(warns)==FALSE){
+        # Print only one of ICCbin calculation warnings
+        for(w in 1:length(multiwarns)){
+          warning(multiwarns[w])
+        }
+      }
+
+
+      ################################################
+      ## Overall X-category resampling ICC estimate ##
+      rhom = mean(rho)
+
+      # Variance & 95% CI of overall resampling estimate
+      vrhom = 1/length(rho)*sum(se^2)
+      rhomci = rhom+c(-1,1)*qnorm(1-alpha/2)*sqrt(vrhom)
+
+      # Check if CIs <0 or >1, reset to 0 or 1
+      if(is.na(rhomci[1])==F & is.na(rhomci[2])==F){
+        if(rhomci[1] < 0 | rhomci[2] > 1) {
+          if(rhomci[1]<0) rhomci[1] = 0
+          if(rhomci[2]>1) rhomci[2] = 1
+          warning("One or both of 'Resampling based' method confidence limits fell outside of [0, 1]")
+        }
+      }
+      else{
+        warning("Resampling based confidence interval for categorical ICC is not estimable")
+      }
+
+      # Check if rhom is missing
+      if(is.na(rhom)==TRUE){
+        warning("Categorical ICC not estimable by 'Resampling' method")
+      }
+      else{
+        # If estimate is <0 or >1 set to missing
+        if (rhom < 0 | rhom > 1) {
+          rhom = "-"
+          warning("Categorical ICC not estimable by 'Resampling' method")
+          # warning("Resampling based confidence interval for ICC is not estimable")
+        }
+      }
+
+      outrm = data.frame("Avg.Resampling.Rho"=c(rhom,sqrt(vrhom),rhomci))
+      rownames(outrm) = c("ICC","se(ICC)","LCL","UCL")
+
+    }
+    if("mom" %in% method){
+      #################################
+      ## X-category MoM ICC estimate ##
+      moms = weirMoM(as.matrix(table(cordata$cid,cordata$y)),se=T)
+      rhomom = moms$theta
+      rhomomse = moms$se
+
+      # 95% CI of overall resampling estimate
+      rhomomci = rhomom+c(-1,1)*qnorm(1-alpha/2)*rhomomse
+
+      # Check if CIs <0 or >1, reset to 0 or 1
+      if (rhomomci[1] < 0 | rhomomci[2] > 1) {
+        if(rhomomci[1]<0) rhomomci[1] = 0
+        if(rhomomci[2]>1) rhomomci[2] = 1
+        warning("One or both of 'Moments' method confidence limits fell outside of [0, 1]")
+      }
+
+      # If estimate is <0 or >1 set to missing
+      if (rhomom < 0 | rhomom > 1) {
+        rhomom = "-"
+        outmom = data.frame("MomentEst.Rho"=c(rhomom,rhomomse,rhomomci))
+        warning("ICC not estimable by 'Moments' Method")
+      }
+      else{
+        outmom = data.frame("MomentEst.Rho"=c(rhomom,rhomomse,rhomomci))
+      }
+      rownames(outmom) = c("ICC","se(ICC)","LCL","UCL")
+    }
+
+    ###########################################################################
+    ## Output final results ##
+    ###########################################################################
+    if(length(method)==1){
+      if(method=="rm") out=outrm
+      else out=outmom
+    }
+    else{
+      out = list(rm=outrm,mom=outmom)
+    }
+  }
+  else{
+    ## Redefine binary outcome ##
+    dt$ysub = ifelse(dt$y==yvals[2],1,0)
+
+    ## Compute binary ICC estimate(s) ##
+    ib = ICCbin::iccbin(cid=cid, y=ysub, data=dt, method=binmethod, ci.type=ci.type,
+                        alpha=alpha, kappa=kappa, nAGQ=nAGQ, M=M)
+
+    ## Output final results ##
+    out=ib
+  }
+
+  options(warn=0)
+  return(out)
+}
+
+### Example call:
+# iccmulti(rho=0.20, prop=c(0.35,0.5,0.15), prvar=0, noc=20, csize=50, csvar=0)
---
+++ R/rccat.R
@@ -0,0 +1,245 @@
+### Code to generate clustered (correlated) multicategory data
+### based on: Biswas A (2004). Generating Correlated Ordinal Random Samples. Stat Probs & Letters
+rccat = function(rho, prop, prvar=0, noc, csize, csvar=0, allevtcl=TRUE,
+                         drawn=10, nowarnings=FALSE){
+  ### Code to generate clustered (correlated) multicategory data ###
+  # rho        = numeric value between 0 and 1 of the assumed ICC value
+  # prop       = numeric vector of each response category's probability, each
+  #               taking value between 0 and 1
+  # prvar      = numeric value or vector of values between 0 and 1 denoting
+  #               percent variation in each assumed event rate, default is 0.
+  # noc        = numeric value of number of clusters to be generated
+  # csize      = numeric value of desired cluster size
+  # csvar      = numeric value between 0 and 1 denoting percent variation in
+  #               cluster sizes, default is 0
+  # allevtcl   = logical value specifying whether all clusters must have all
+  #               categories, default is TRUE
+  # drawn      = maximum number of attempts to apply variation to event
+  #               probabilities
+  # nowarnings = flag to turn off warnings. Default is False.
+
+  if(noc<1){
+    stop("Number of clusters must be at least 2.")
+  }
+  if(csvar<0 | csvar>1){
+    stop("Cluster variation must be within [0,1].")
+  }
+  if(sum(sapply(prvar, function(x) (x<0 | x>1)))>0){
+    stop("Event rate variation must be within [0,1].")
+  }
+  if(length(prop)>1 & sum(prop)!=1){
+    stop("Categorical event rate probabilities must sum to 1.")
+  }
+
+  ## If prop is a single number, assume event rate for binary response
+  if(length(prop)==1) prop = c(1-prop,prop)
+
+
+  ## set correlation rho = (b/(sum(a_u) + b))^2 --> b = sqrt(rho)/(1-sqrt(rho))
+  b = sqrt(rho)/(1-sqrt(rho))
+  out = NULL
+
+  ## Initialize vector of cluster ids
+  cluster <- c()
+
+  ## Initialize categorical outcome vector
+  y <- c()
+
+
+  ## Save warnings
+  rcwarn = NULL
+
+
+  ################################################
+  ### Iterate following steps for each cluster ###
+  ################################################
+  for (i in 1:noc) {
+    ###### Induce variation in cluster size ######
+
+    ## Define min cluster size as (1-csvar)*csize or at least # categories
+    min_csize <- ifelse((csize - round(csize * csvar)) >=
+                          length(prop), csize - round(csize * csvar), length(prop))
+
+    ## Define cluster size of current [i] cluster
+    csizen <- abs(round(csize + (csize * csvar) * rnorm(1)))
+
+    ## If too small, draw again
+    while (csizen < min_csize) {
+      csizen <- abs(round(csize + (csize * csvar) * rnorm(1)))
+    }
+
+
+    ###### Induce variation in event probabilities ######
+
+    ##~~ If all event probs have same variation percentage ~~##
+    if(length(prvar)==1){
+
+      # Flag for probability (re)draws
+      drawp=drawcount=1
+
+      ## Define min event proportion: (1-prvar)*prop or at least 0
+      min_pr <- sapply(prop, function(a)
+        ifelse((a - a * prvar) >= 0, a - a * prvar, 0))
+
+      ## Define max event proportion: (1+prvar)*prop or no more than 1
+      max_pr <- sapply(prop, function(a)
+        ifelse((a + a * prvar) <= 1, a + a * prvar, 1))
+
+      ## Create new event probability vector
+      prn = prop
+
+      while(drawp==1 & drawcount<drawn){
+        # Update all but one probability to ensure all sum to 1
+        updatep = sample(1:length(prop),length(prop)-1)
+        for(k in updatep){
+          ## Define event proportion within current [i] cluster
+          prn[k] <- abs(prop[k] + (prop[k] * prvar) * rnorm(1))
+          ## If too small or too big, draw again
+          while (prn[k] < min_pr[k] | prn[k] > max_pr[k]) {
+            prn[k] <- abs(prop[k] + (prop[k] * prvar) * rnorm(1))
+          }
+        }
+        # Update final category
+        prn[-updatep] = 1-sum(prn[updatep])
+        if(min_pr[-updatep]<=prn[-updatep] & prn[-updatep]<=max_pr[-updatep]){
+          drawp=0
+        }
+        drawcount = drawcount+1
+      }
+
+    }else{
+      # Flag for probability (re)draws
+      drawp=drawcount=1
+
+      # Update all but one probability to ensure all sum to 1
+      updatep = sample(1:length(prop),length(prop)-1)
+      ##~~ event probs with distinct variation percentages ~~##
+      prn = prop
+      while(drawp==1 & drawcount<drawn){
+        for(k in updatep){
+          ## Define min event proportion: (1-prvar)*prop or at least 0
+          min_pr <- ifelse((prop[k] - prop[k] * prvar[k]) >= 0, prop[k] -
+                             prop[k] * prvar[k], 0)
+          ## Define max event proportion: (1+prvar)*prop or no more than 1
+          max_pr <- ifelse((prop[k] + prop[k] * prvar[k]) <= 1, prop[k] +
+                             prop[k] * prvar[k], 1)
+          ## Define event proportion within current [i] cluster
+          prn[k] <- abs(prop[k] + (prop[k] * prvar[k]) * rnorm(1))
+          ## If too small or too big, draw again
+          while (prn[k] < min_pr | prn[k] > max_pr) {
+            prn[k] <- abs(prop[k] + (prop[k] * prvar[k]) * rnorm(1))
+          }
+        }
+
+        ## Update final category
+        ## Define min event proportion: (1-prvar)*prop or at least 0
+        min_pr <- ifelse((prop[-updatep] - prop[-updatep] * prvar[-updatep])>=0,
+                         prop[-updatep] - prop[-updatep] * prvar[-updatep], 0)
+        ## Define max event proportion: (1+prvar)*prop or no more than 1
+        max_pr <- ifelse((prop[-updatep] + prop[-updatep] * prvar[-updatep])<=1,
+                         prop[-updatep] + prop[-updatep] * prvar[-updatep], 1)
+        # Define final category probability
+        prn[-updatep] = 1-sum(prn[updatep])
+        # Check it falls within its min & max pr
+        if(min_pr<= prn[-updatep] & prn[-updatep]<= max_pr){ drawp=0}
+
+        drawcount = drawcount+1
+      }
+    }
+    if(drawp==1 & prvar!=0){
+      rcwarn = c(rcwarn,"Limit reached when varying event probabilities. Probabilities reset to original values with 0% variation.")
+      # Reset probabilities
+      prn = prop
+    }
+
+
+    #################################################################
+    ######                Biswas A3 algorithm                  ######
+    ###### (see pseudocode in Chakraborty Comm. in Stats 2023) ######
+    #################################################################
+
+    #######################################
+    ### Step 1: Set prob distribution of random effect Y0 to desired prob distn
+    # Calculate cumulative probability distribution
+    cdf0 = c(0,cumsum(prn))
+
+    #######################################
+    ### Step 2: draw random effect Y0 ###
+    # Draw a random number between [0,1]
+    r0 = runif(1)
+    # For k'=0,1,...,length(prop)-1 set y0 = k' if cdf0[k-1] < r0 <= cdf0[k], k' = k-1
+    #   where 0,...length(prop)-1 are (the desired values of final outcome y - 1)
+    y0 = seq(0,length(prn)-1,1)[sapply(1:(length(cdf0)-1),
+                                       function(x) (cdf0[x]<r0 & r0<=cdf0[x+1]))]
+
+    #######################################
+    ### Step 3: Update probability distribution and draw outcome values ###
+    # Initialize update prob distn to current prob
+    prnew = prn
+
+    ## Update the probability corresponding to value y0:
+    # The prob corresponding to category [k] that y0 equals is now =
+    #     (Pr(k)+b)/(sum(prop)+b)
+    prnew[which(seq(0,length(prn)-1,1)==y0)] =
+      ( prn[which(seq(0,length(prn)-1,1)==y0)] + b ) / ( sum(prn)+b )
+
+    # Update the remaining probabilities
+    # Those prop not corresponding to category [k] = y0 are now =
+    #     Pr([-k])/(sum(prop)+b)
+    prnew[which(seq(0,length(prn)-1,1)!=y0)] =
+      sapply(prn[which(seq(0,length(prn)-1,1)!=y0)],
+             function(x) x / ( sum(prn)+b ) )
+
+    # Define cumulative prob distn of multicategory outcome for cluster [i]
+    cdfi = c(0,cumsum(prnew))
+
+
+    #################################
+    ### Draw categorical outcomes ###
+    #################################
+    yi = clusteri = NULL
+    for(j in 1:csizen){
+      rj = runif(1)
+      # Set individual j to category [k] if cdfi[k-1] < rj <= cdfi[k]
+      yij = seq(0,length(prn)-1,1)[sapply(1:(length(cdfi)-1),
+                                          function(x) (cdfi[x]<rj & rj<=cdfi[x+1]))]
+      yi = c(yi,yij)
+      # Repeat cluster ID for all individual values
+      clusteri=c(clusteri,i)
+    }
+    ### If must have all 3 categories in all clusters
+    if(allevtcl==TRUE & length(unique(yi))<length(prn)){
+      while(length(unique(yi))<length(prn)){
+        # Drop previous pulls of y
+        yi = NULL
+
+        for(j in 1:csizen){
+          rj = runif(1)
+          # Set individual j to category [k] if cdfi[k-1] < rj <= cdfi[k]
+          yij = seq(0,length(prn)-1,1)[sapply(1:(length(cdfi)-1),
+                                              function(x) (cdfi[x]<rj & rj<=cdfi[x+1]))]
+          yi = c(yi,yij)
+        }
+      }
+    }
+    y = c(y,yi)
+    cluster = c(cluster,clusteri)
+  }
+
+  # Print only one of ICCbin calculation warnings
+  if(is.null(rcwarn)==FALSE & nowarnings==FALSE){
+    rcmultiwarns = unique(rcwarn)
+    for(w in 1:length(rcmultiwarns)){
+      warning(rcmultiwarns[w])
+    }
+  }
+
+  out = data.frame(cid=as.factor(cluster),y=y)
+  if(length(prop)>2) out$y = out$y+1
+  return(out)
+}
+
+### Example call:
+# rccat(rho=0.20, prop=c(0.35,0.5,0.15), prvar=0, noc=20, csize=50, csvar=0)
+
+
